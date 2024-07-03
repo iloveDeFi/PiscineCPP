f12ad934e9f1b5c27fd121a3a311624aaa9ad0a4
Baptiste Bessard <bbessard@c3r3s2.42lausanne.ch>
2024-07-03 12:09:01 +0200

    ex02

--- 00/ex01/Contact.class.h
+++ 00/ex01/Contact.class.h
@@ -6,42 +6,48 @@
 /*   By: bbessard <bbessard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/01 14:42:35 by bbessard          #+#    #+#             */
-/*   Updated: 2024/07/01 15:22:07 by bbessard         ###   ########.fr       */
+/*   Updated: 2024/07/03 11:57:47 by bbessard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+// DECLARATION OF MY CLASS
+// (POD CLASS - Plain Old Data)
 
 #ifndef CONTACT_CLASS_H
 # define CONTACT_CLASS_H
 
+using std::string;
+using std::integer;
+
+// Canonical form for my class Contact
 class Contact {
     private:
-        char* name;
-    protected:
-    public:
-        // Canonical form for my class
-        // Constructuer par defaut (CD)
-        Contact() : name(nullptr) {
-            std::cout << "Default constructor called" << std::endl;
-        }
-        // Constructeur parametre (CP)
-        Contact(const char *name)
-        {
-            std::cout << "Param constructor called" << std::endl;
-            this->name = new char[strlen(name) + 1];
-            strcpy(this->name, name);
-        }
-        // Constructuer de copie (CC)
-
-
-        // Operateur daffectation par copie (OAC)
-
+        string _phone_number;
+        string _darkest_secret;
+        string _first_name;
+        string _last_name;
+        string _nickname;
+        integer _index;
         
-        // Destructeur (D)
-        ~Contact()
-        {
-            std::cout << "Destructor called" << std::endl;
-            delete[] name;
-        }
+    public:
+        // Constructor by default
+        Contact (void);
+
+        // Constructor by copie
+        Contact (const Contact& other);
+
+        // Assignement Operator
+        Contact & operator=(Contact const & rhs);
+        Contact & operator+(Contact const & rhs) const;
+
+        // Destrcutor = EXIT && DELETE
+        ~Contact (void);
+        
+        // Public Methodes
+        // Setter = ADD
+        void setNewContact(const string& firstName);
+        // Getter = SEARCH
+        string getContact() const;
 };
 
 #endif
